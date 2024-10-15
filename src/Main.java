d3c90c90f984845a0deaa27ed8d6f609c7180dc9
dunglt94 <tuandung9594@gmail.com>
2024-10-15 16:10:23 +0700

    first add

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Dung");
+        s1.setClasses("C0824");
+        System.out.println(s1.getName());
+        System.out.println(s1.getClasses());
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Student() {}
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
+    public String getClasses() {
+        return classes;
+    }
+    public void setClasses(String newClasses) {
+        classes = newClasses;
+    }
+}
